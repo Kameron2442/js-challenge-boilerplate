2668307cebea4a8cb7c6976a0fc57db13536758a
Kameron B. <56085171+Kameron2442@users.noreply.github.com>
2025-09-22 22:14:39 -0400

    Story-1: Added elements in the nav bar and policy reader

--- src/app/components/pages/policy-reader/policy-reader.component.ts
+++ src/app/components/pages/policy-reader/policy-reader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomePolicy } from '../../../interfaces/policy';
 
 @Component({
     selector: 'app-policy-reader',
@@ -7,5 +8,33 @@ import { Component } from '@angular/core';
     styleUrl: './policy-reader.component.scss'
 })
 export class PolicyReaderComponent {
+
+    policy1: HomePolicy = {
+        id: 0,
+        policyNumber: 100,
+        valid: false
+    }
+
+    policy2: HomePolicy = {
+        id: 0,
+        policyNumber: 200,
+        valid: false
+    }
+
+    policy3: HomePolicy = {
+        id: 0,
+        policyNumber: 300,
+        valid: false
+    }
+
+    policyNumbers: HomePolicy[] = [
+        this.policy1, this.policy2, this.policy3
+    ]
+
+    readCSV(event: Event): void {
+     
+    }
+
+
 
 }
