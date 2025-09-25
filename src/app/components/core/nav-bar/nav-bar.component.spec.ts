239e0e4521302797ab010e68685df8c8cb3870ea
Kameron B. <56085171+Kameron2442@users.noreply.github.com>
2025-09-25 19:01:45 -0400

    Story-1: Nav bar refinements

--- src/app/components/core/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/core/nav-bar/nav-bar.component.spec.ts
@@ -4,6 +4,7 @@ import { NavBarComponent } from './nav-bar.component';
 
 describe('NavBarComponent', () => {
     let component: NavBarComponent;
+    let componentElement: HTMLElement;
     let fixture: ComponentFixture<NavBarComponent>;
 
     beforeEach(async () => {
@@ -14,10 +15,26 @@ describe('NavBarComponent', () => {
 
         fixture = TestBed.createComponent(NavBarComponent);
         component = fixture.componentInstance;
+        componentElement = fixture.debugElement.nativeElement;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should have a title', () => {
+        const siteTitle = componentElement.querySelector('.nav-bar__title')!;
+        expect(siteTitle.textContent).toEqual(`Kameron's Code Challenge`);
+    });
+
+    it('should flip the theme boolean when the theme button is clicked', () => {
+        const themeButton = componentElement.querySelector('.nav-bar__theme-button') as HTMLButtonElement;
+        expect(component.showLightTheme()).toBeFalse();
+        themeButton.click();
+        expect(component.showLightTheme()).toBeTruthy();
+        themeButton.click();
+        expect(component.showLightTheme()).toBeFalse();
+    });
+    
 });
--- src/app/components/core/nav-bar/nav-bar.component.ts
+++ src/app/components/core/nav-bar/nav-bar.component.ts
@@ -1,21 +1,22 @@
-import { Component, inject, Renderer2 } from '@angular/core';
+import { Component, inject, Renderer2, signal } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-nav-bar',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
 
+    public showLightTheme = signal(false);
     private document = inject(DOCUMENT);
     private renderer = inject(Renderer2);
-    protected showLightTheme: boolean = false;
 
-    protected flipTheme(): void {
-        this.showLightTheme = !this.showLightTheme;
-        this.showLightTheme ? this.setTheme('light') : this.setTheme('dark');
+    public flipTheme(): void {
+        this.showLightTheme.set(!this.showLightTheme());
+        this.showLightTheme() ? this.setTheme('light') : this.setTheme('dark');
     }
 
     private setTheme(theme: string): void {
